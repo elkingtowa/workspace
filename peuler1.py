85f055ab62ea4aef33efdd1548add7f737e5fa7d
jme <elkingtonx@gmail.com>
2014-07-11 00:02:28 -0400

    project euler

---
+++ peuler1.py
@@ -0,0 +1,27 @@
+#Sum of 3, 5, 6 and 9 is 23.
+
+three = 3
+five = 5
+six = 6
+nine = 9
+
+sums = three + five + six + nine
+
+#Find the sum of all the multiples of 3 or 5 below 1000
+
+num = 1000
+lis = {}
+
+for i in range(num):
+	if i % 3 == 0:
+
+		lis[i] =  i
+
+	elif i % 5 == 0:
+
+		lis[i] = i
+
+lis = sorted(set(lis))
+print lis
+suml = sum(lis)
+print suml
