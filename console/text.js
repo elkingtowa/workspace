7b4c3246e064ce8dd6faab820bc7e3c51023cf2f
jme <elkingtonx@gmail.com>
2014-08-24 22:15:52 -0400

    console

---
+++ console/text.js
@@ -0,0 +1,66 @@
+var text = {
+help: "Commands:\n\
+  - help: console options\n\
+  - console: a description of console\n\
+  - info [function]: information on a console function\n\
+  - issues: information on console issues\n\
+  - contact: console email information\n\n\
+This web site was built with <a href='http://replit.github.io/jq-console/' target='_blank'>JQconsole</a>.",
+console: "Neu Venture Capital is an early stage investor based out of New York City. We like to be the first\n\
+money into a company. We both lead and follow into investment rounds and can help syndicate. We like\n\
+to be involved with our companies, sometimes accepting seats on the board, but always available to help.\n\n\
+We are small, but we think big. We like ambitious companies in big markets with founders who can't\n\
+help but try to change the world.",
+people: "Jerry Neumann is a veteran venture investor, investing in internet startups for the past\n\
+seventeen years from his home base in New York City. He is a mentor at TechStars NY, Women Innovate\n\
+Mobile, and SeedStart. He has sat on the boards of Lucky Sort, Magnetic, Pinch Media, PlaceIQ, Simple,\n\
+Tuition.io, and Yieldbot. He is an advisor to BigML, Ginzametrics, Luma Partners, and Sociocast. He is a\n\
+adjunct professor at Columbia University, teaching entrepreneurship at the engineering school.\n\n\
+Jerry grew up in NJ and went to Columbia for his bachelor's and master's in electrical engineering.\n\
+His first job was as an engineer at IBM, where he designed a very small part of the CP microcode\n\
+execution engine for the S/390 series of mainframes. He left to get his master's in business from NYU.\n\n\
+After school he went to work for Deloitte & Touche Consulting, ending up in their Restructuring\n\
+Services Group. He joined Prodigy Services in 1995, and was part of the senior management team that\n\
+successfully transformed Prodigy from a walled garden online service to an internet-centric ISP and portal.\n\n\
+Jerry left Prodigy to run Omnicom Group's internet division/corporate venture capital arm, Communicade.\n\
+Communicade seed funded the best early interactive agencies, including Razorfish, Agency.com,\n\
+Organic Interactive, Red Sky, and others.\n\n\
+In 2001, Jerry formed Seneca Investments and acquired the Communicade portfolio from Omnicom. Seneca\n\
+rode out the downturn and emerged with several great companies, including the newly privatizedAgency.com\n\
+and Organic (later acquired by Omnicom), Taleo (IPOd), Oyster Interactive (acquired by FramFab),\n\
+Healthology (acquired by iVillage), eMedicine (acquired by WebMD), and Domain (acquired by Melbourne IT).\n\n\
+Jerry then co-founded Root Markets, and helped pioneer the idea of applying the techniques and tools\n\
+of financial markets to marketing services. Backed by First Round Capital, the Chicago Board of Trade,\n\
+and other top-notch investors, but way ahead of its time, Root did not survive the collapse of the\n\
+mortgage markets. Jerry started Neu Venture Capital in 2008.\n\n\
+Jerry has three wonderful kids and lives in Hoboken, NJ. When he's not working or with his loved ones\n\
+he likes to be outdoors, skiing, kayaking, backpacking, or rock climbing, depending on the weather.",
+thesis: "Neu believes that real value is created when a company makes life better for its customers.\n\
+We like to support companies using high technology to create systemic change. Companies we have\n\
+invested in use real-time interaction, big data, machine learning, data visualization, and mobile\n\
+technologies to radically modernize the advertising, media, retail, education, and finance sectors.",
+labs: "Stuff we've built.\n\n\
+  VCdelta\n\
+  Daily alerts about new companies added to VC firms' portfolio pages.\n\
+  Twitter: <a href='http://twitter.com/VCdelta' target='_blank'>@VCdelta</a>\n\
+  Website: <a href='http://neuvc.com/labs/vcdelta/' target='_blank'>neuvc.com/labs/vcdelta/</a>\n\n\
+  VCbar\n\
+  A graphical visualization of VC investing over time, from Crunchbase data.\n\
+  Website: <a href='http://neuvc.com/labs/vcbar/' target='_blank'>neuvc.com/labs/vcbar/</a>\n\n\
+  CoViz\n\
+  Find the VCs that are central to your industry by creating a network graph of\n\
+  company/VC relationships.\n\
+  Website: <a href='http://neuvc.com/labs/coviz/' target='_blank'>neuvc.com/labs/coviz/</a>\n\n\
+  VC Genealogy\n\
+  Shows the history and personal interconnectivity of seminal VC firms.\n\
+  Website: <a href='http://neuvc.com/labs/vcgenealogy/' target='_blank'>neuvc.com/labs/vcgenealogy/",
+links: "The blog:  <a href='http://reactionwheel.net' target='_blank'>reactionwheel.net</a>\n\
+Twitter:   <a href='http://twitter.com/ganeumann' target='_blank'>@ganeumann</a>\n\
+AngelList: <a href='https://angel.co/ganeumann' target='_blank'>angel.co/ganeumann</a>\n\
+LinkedIn:  <a href='https://www.linkedin.com/in/jerryneumann' target='_blank'>linkedin.com/in/jerryneumann</a>",
+ls: "companies          help                     console\n\
+contact                 info                     people\n\
+exits                   labs                     thesis\n\
+goto                    links",
+easteregg: "Try 'xyzzy'."
+};
